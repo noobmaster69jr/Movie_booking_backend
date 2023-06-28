0afe20c7d7d3c7f6535f402487aeb4816fa97e7a
noobmaster69jr <srashwin22@gmail.com>
2023-06-28 10:58:18 +0530

    added middleware for validation

--- routes/movie.routes.js
+++ routes/movie.routes.js
@@ -1,7 +1,8 @@
 const movieController = require("../controllers/movie.controller");
+const validateMovieRequest = require("../middlewares/validateMovieRequest");
 
 module.exports = function (app) {
-  app.post("/mba/api/v1/movies", movieController.createMovie);
+  app.post("/mba/api/v1/movies", validateMovieRequest, movieController.createMovie);
   app.get("/mba/api/v1/movies", movieController.getAllMovies);
    app.get("/mba/api/v1/movies/:id", movieController.getMovie);
   app.put("/mba/api/v1/movies/:id", movieController.updateMovie);
