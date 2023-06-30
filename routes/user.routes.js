d83b6a78804ad66a48b561de8a288a034434333b
noobmaster69jr <srashwin22@gmail.com>
2023-06-30 17:15:18 +0530

    added update password and authJWT middleware

---
+++ routes/user.routes.js
@@ -0,0 +1,8 @@
+const userController = require("../controllers/user.controller");
+const { verifyToken } = require("../middlewares/authJWT");
+
+module.exports = function (app) {
+  app.put("/mba/api/v1/users", [verifyToken], userController.update);
+};
+
+
