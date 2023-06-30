5386e426d81689d9e01864f31c30fb2ebe7f531b
noobmaster69jr <srashwin22@gmail.com>
2023-06-30 17:27:42 +0530

    approve admin user

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,8 +1,13 @@
 const userController = require("../controllers/user.controller");
-const { verifyToken } = require("../middlewares/authJWT");
+const { verifyToken, isAdmin } = require("../middlewares/authJWT");
 
 module.exports = function (app) {
   app.put("/mba/api/v1/users", [verifyToken], userController.update);
+  app.put(
+    "/mba/api/v1/users/:userId",
+    [verifyToken, isAdmin],
+    userController.updateUser
+  );
 };
 
 
--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const bcrypt = require("bcrypt");
+
 
 const serverConfig = require("./configs/server.config");
 const dbConfig = require("./configs/db.config");
+const User = require("./models/user.model");
+const constants = require("./utils/constants");
 
 const app = express();
 
@@ -14,8 +18,9 @@ mongoose.connect("mongodb://localhost:27017/mba_db")
 const db = mongoose.connection
 db.on("error", () => console.log("Can't connect to DB"));
 db.once("open", () => {
+  
   console.log("Connected to Mongo DB");
-
+  init();
 });
 
 
@@ -31,3 +36,21 @@ app.get("/", (req, res) => {
 app.listen(serverConfig.PORT, () => {
   console.log(`Application running on port ${serverConfig.PORT}`);
 });
+
+
+async function init() {
+  try {
+    const user = await User.create({
+      name: "admin",
+      userId: "admin",
+      email: "admin@gmail.com",
+      password: bcrypt.hashSync("admin", 10),
+      userStatus: constants.userStatus.approved,
+      userTypes: constants.userTypes.admin,
+    });
+
+    console.log("Admin user created successfully");
+  } catch (e) {
+    console.log(e.message);
+  }
+}
