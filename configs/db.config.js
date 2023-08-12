8261ac7a41dd55e59612a12a514aa7fd3ce3fd39
noobmaster69jr <66981286+noobmaster69jr@users.noreply.github.com>
2023-08-12 16:33:58 +0530

    Update db.config.js

--- configs/db.config.js
+++ configs/db.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   DB_NAME: "mba_db",
   DB_URL:
-   "mongodb+srv://moviebooking81:mangodb@81@cluster0.rrmd9ka.mongodb.net/?retryWrites=true&w=majority"
+   "mongodb+srv://noobmaster:noobmaster@cluster0.grszsip.mongodb.net/"
 };
 
